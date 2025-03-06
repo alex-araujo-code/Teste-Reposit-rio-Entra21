655ed0f9c805356687d2d5e25448a0fe483896a4
Alex Araujo <alexgabriel.araujo@hotmail.com>
2025-03-06 09:11:17 -0300

    Introdução a Orientação a Objetos.

---
+++ Projeto Teste Entra21 GitHub/Funcionario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Teste_Entra21_GitHub
+{
+    public class Funcionario
+    {
+        public string Nome;
+        public int Idade;
+        public string Cargo;
+        public double Salario;
+
+        public void Apresentacao()
+        {
+            Console.WriteLine($"""
+
+                Funcionario:
+                
+                Nome: {this.Nome}
+                Idade: {this.Idade}
+                Cargo: {this.Cargo}
+                Salário: {this.Salario}
+                Salário Líquido: {SalarioLiquido()}
+                """);
+        }
+
+        public double SalarioLiquido()
+        {
+            if (Cargo == "Estagiário")
+                return Salario;
+            return Salario * 0.73;
+        }
+    }
+}
--- Projeto Teste Entra21 GitHub/Program.cs
+++ Projeto Teste Entra21 GitHub/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Projeto_Teste_Entra21_GitHub
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Projeto_Teste_Entra21_GitHub
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Pessoa pessoa = new Pessoa();
+            pessoa.Nome = "Alex";
+            pessoa.Idade = 20;
+            Console.WriteLine($"Nome: {pessoa.Nome}\nIdade: {pessoa.Idade}");
+
+            pessoa.DizerOi();
+
+            Funcionario alex = new Funcionario();
+            alex.Nome = "Alex";
+            alex.Idade = 20;
+            alex.Cargo = "Estagiário";
+            alex.Salario = 7390.99;
+
+            Funcionario chefe = new Funcionario();
+            chefe.Nome = "Gab";
+            chefe.Idade = 32;
+            chefe.Cargo = "Chefe de Setor";
+            chefe.Salario = 25666.01;
+
+            alex.Apresentacao();
+            chefe.Apresentacao();
         }
     }
 }
